13754500b3d3cfdc40f5284eaa745f03907136a5
James Harrison <james.harrison@satellitevu.com>
2025-02-28 15:55:16 +0000

    otm, cos, policy config

--- builder/config.py
+++ builder/config.py
@@ -3,4 +3,7 @@ BASE_URL = "https://api.satellitevu.com/"
 APIS: dict[str, str] = {
     "catalog": "/catalog/v1",
     "id": "/id/v2",
+    "otm": "/otm/v2",
+    "cos": "/orders/v2",
+    "policy": "/policy/v1"
 }

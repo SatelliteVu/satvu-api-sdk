2666adfee5e0a0cc30a2d753302df29429fedea6
James Harrison <james.harrison@satellitevu.com>
2025-05-16 17:27:09 +0100

    add resellers api to config

--- builder/config.py
+++ builder/config.py
@@ -2,8 +2,9 @@ BASE_URL = "https://api.satellitevu.com/"
 
 APIS: dict[str, str] = {
     "catalog": "/catalog/v1",
-    "id": "/id/v2",
-    "otm": "/otm/v2",
     "cos": "/orders/v2",
-    "policy": "/policy/v1"
+    "id": "/id/v2",
+    "policy": "/policy/v1",
+    "otm": "/otm/v2",
+    "resellers": "/resellers/v1",
 }

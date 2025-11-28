95f3095abdcdc1e24efec640f18b63c6865034c2
James Harrison <james.harrison@satellitevu.com>
2025-11-28 15:52:31 +0000

    feat: use id api v3

--- src/builder/config.py
+++ src/builder/config.py
@@ -3,7 +3,7 @@ BASE_URL = "https://api.qa.satellitevu.com/"
 APIS: dict[str, str] = {
     "catalog": "/catalog/v1",
     "cos": "/orders/v3",
-    "id": "/id/v2",
+    "id": "/id/v3",
     "policy": "/policy/v1",
     "otm": "/otm/v2",
     "reseller": "/resellers/v1",
